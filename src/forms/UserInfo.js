15f7e88f2ac674dbfccc2b40f216ed6845979c19
cdalangin <florenzdalangin@gmail.com>
2020-08-15 10:51:05 -0400

    log in and sign up

--- src/forms/UserInfo.js
+++ src/forms/UserInfo.js
@@ -1,33 +1,62 @@
 import React from 'react';
+import Tabs from 'react-bootstrap/Tabs';
+import Tab from 'react-bootstrap/Tab';
 import { Form } from 'react-bootstrap';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
+import './UserInfo.scss'
 
 export function UserInfo() {
     return (
-        <Card style={{ width: '18rem' }}>
-            <Form>
-                <Form.Group controlId="formName">
-                    <Form.Label>Full Name</Form.Label>
-                    <Form.Control type="text" placeholder="Enter Full Name" />
-                </Form.Group>
+        <Tabs defaultActiveKey="profile" id="uncontrolled-tab-example">
+            <Tab eventKey="home" title="Sign Up">
+                <Card className='userForm'>
+                    <Card.Title>Create a new account</Card.Title>
+                    <Form>
+                        <Form.Group controlId="formName" class="">
+                            <Form.Label>Full Name</Form.Label>
+                            <Form.Control type="text" placeholder="Enter Full Name" />
+                        </Form.Group>
 
-                <Form.Group controlId="formEmail">
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
-                    <Form.Text className="text-muted">
-                        We'll never share your email with anyone else.
-                    </Form.Text>
-                </Form.Group>
+                        <Form.Group controlId="formUserName" class="">
+                            <Form.Label>Username</Form.Label>
+                            <Form.Control type="text" placeholder="Enter Username" />
+                        </Form.Group>
 
-                <Form.Group controlId="formPassword">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
-                </Form.Group>
-                <Button variant="primary" type="submit">
-                    Submit
+                        <Form.Group controlId="formEmail">
+                            <Form.Label >Email address</Form.Label>
+                            <Form.Control type="email" placeholder="Enter email" />
+                        </Form.Group>
+
+                        <Form.Group controlId="formPassword">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type="password" placeholder="Password" />
+                        </Form.Group>
+                        <Button variant="primary" type="submit">
+                            Sign Up
                 </Button>
-            </Form>
-        </Card>
+                    </Form>
+                </Card>
+            </Tab>
+            <Tab eventKey="profile" title="Log In">
+                <Card className='userForm'>
+                    <Card.Title>Welcome Back!</Card.Title>
+                    <Form>
+                        <Form.Group controlId="formEmail">
+                            <Form.Label>Email address</Form.Label>
+                            <Form.Control type="email" placeholder="Enter email" />
+                        </Form.Group>
+
+                        <Form.Group controlId="formPassword">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type="password" placeholder="Password" />
+                        </Form.Group>
+                        <Button variant="primary" type="submit">
+                            Login
+                        </Button>
+                    </Form>
+                </Card>
+            </Tab>
+        </Tabs>
     )
 }
