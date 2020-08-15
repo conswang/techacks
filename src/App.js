907c5885d8698dd94805cae013ee088e5f3f4984
Cons Wang <conswang@seas.upenn.edu>
2020-08-15 11:43:50 -0400

    added user provider

--- src/App.js
+++ src/App.js
@@ -4,15 +4,16 @@ import { BookListingForm } from './forms/BookListingForm';
 import { Note } from './components/Note'
 import { mockNote } from './mockData.js'
 import { UserInfo } from './forms/UserInfo'
+import UserProvider from './providers/UserProvider'
 
 function App() {
   return (
-    <>
+    <UserProvider>
       <NoteListingForm></NoteListingForm>
       <BookListingForm></BookListingForm>
       <UserInfo></UserInfo>
       <Note note={mockNote}></Note>
-    </>
+    </UserProvider>
   );
 }
 
