b80e8b53ff6d819c5c191b88e592d617b22dc3be
Joytaylor <jtaylor2@imsa.edu>
2020-08-15 10:55:45 -0500

    updated app
    now recieves data from firebase

--- src/App.js
+++ src/App.js
@@ -1,19 +1,41 @@
-import React from 'react';
-import { NoteListingForm } from './forms/NoteListingForm';
-import { BookListingForm } from './forms/BookListingForm';
-import { Note } from './components/Note'
-import { mockNote } from './mockData.js'
-import { UserInfo } from './forms/UserInfo'
+import React, { Component, useState } from "react";
+import { NoteListingForm } from "./forms/NoteListingForm";
+import { BookListingForm } from "./forms/BookListingForm";
+import { Note } from "./components/Note";
+import { mockNote } from "./mockData.js";
+import { UserInfo } from "./forms/UserInfo";
+import axios from "axios";
 
-function App() {
-  return (
-    <>
-      <NoteListingForm></NoteListingForm>
-      <BookListingForm></BookListingForm>
-      <UserInfo></UserInfo>
-      <Note note={mockNote}></Note>
-    </>
-  );
+class App extends Component {
+  state = {
+    notes: null,
+  };
+  componentDidMount() {
+    axios
+      .get("/get5Notes")
+      .then((res) => {
+        this.setState({
+          notes: res.data,
+        });
+      })
+      .catch((err) => console.log(err));
+  }
+  render() {
+    let notesloaded = this.state.notes ? (
+      this.state.notes.map((notes) => <Note note={notes} />)
+    ) : (
+      <p>Getting Notes... </p>
+    );
+    return (
+      <>
+        <NoteListingForm></NoteListingForm>
+        <BookListingForm></BookListingForm>
+        <UserInfo></UserInfo>
+        <Note note={mockNote}></Note>
+        {notesloaded}
+      </>
+    );
+  }
 }
 
 export default App;
