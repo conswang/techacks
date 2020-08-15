e79a1ad20d27653a455ab0d072ab1e78c4820f1c
Cons Wang <conswang@seas.upenn.edu>
2020-08-15 10:34:40 -0400

    note state

--- src/forms/NoteListingForm.js
+++ src/forms/NoteListingForm.js
@@ -1,47 +1,59 @@
 import React from 'react';
-import { Form, Col } from "react-bootstrap";
+import { Form, Col, Row } from "react-bootstrap";
 import Button from 'react-bootstrap/Button';
+// import { api } from 'functions/src/index.ts';
 
 export function NoteListingForm() {
+  const [title, setTitle] = React.useState('');
+  const [subject, setSubject] = React.useState('');
+  const [course, setCourse] = React.useState('');
+  const [description, setDescription] = React.useState('');
+  const [ask, setAsk] = React.useState('');
+  const [price, setPrice] = React.useState();
+
+  const onSubmit = () => {
+    
+  }
+
   return(
     <Form>
       <Form.Group>
         <Form.Label>Title</Form.Label>
-        <Form.Control></Form.Control>
+        <Form.Control onChange={e => setTitle(e.target.value)}></Form.Control>
       </Form.Group>
 
-      <Form.Row>
+      <Row>
         <Form.Group as={Col}>
           <Form.Label>Subject</Form.Label>
-          <Form.Control></Form.Control>
+          <Form.Control onChange={e => setSubject(e.target.value)}></Form.Control>
         </Form.Group>
         <Form.Group as={Col}>
           <Form.Label>Course</Form.Label>
-          <Form.Control></Form.Control>
+          <Form.Control onChange={e => setCourse(e.target.value)}></Form.Control>
         </Form.Group>
-      </Form.Row>
+      </Row>
 
       <Form.Group>
         <Form.Label>Description</Form.Label>
-        <Form.Control as="textarea" rows="3"></Form.Control>
+        <Form.Control as="textarea" rows="3" onChange={e => setDescription(e.target.value)}></Form.Control>
       </Form.Group>
 
-      <Form.Row>
+      <Row>
         <Form.Group as={Col}>
           <Form.Label>Ask</Form.Label>
-          <Form.Control></Form.Control>
+          <Form.Control onChange={e => setAsk(e.target.value)}></Form.Control>
         </Form.Group>
         <Form.Group as={Col}>
           <Form.Label>Price</Form.Label>
-          <Form.Control></Form.Control>
+          <Form.Control onChange={e => setPrice(e.target.value)}></Form.Control>
         </Form.Group>
-      </Form.Row>
+      </Row>
 
       <Form.Group>
         <Form.File label="Upload one page preview"/>
       </Form.Group>
 
-      <Button>Submit</Button>
+      <Button onClick={onSubmit}>Submit</Button>
     </Form>
   )
 }
