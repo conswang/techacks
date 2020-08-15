b41e8c186e0d80d1547e4c7a7b7ecc5c18bcf835
Joytaylor <jtaylor2@imsa.edu>
2020-08-15 01:56:56 -0500

    functions with express

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,91 @@
 import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+import * as express from "express";
+const app = express();
 
+admin.initializeApp();
+admin.firestore().settings({ ignoreUndefinedProperties: true });
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
 
-export const helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Hello from Firebase!");
+app.get("/get5Notes", (req, res) => {
+  admin
+    .firestore()
+    .collection("notes")
+    .limit(5)
+    .orderBy("created", "desc")
+    .get()
+    .then((data) => {
+      let users: any = [];
+      data.forEach((doc) => {
+        users.push({
+          nId: doc.id,
+          ...doc.data(),
+        });
+      });
+      return res.json(users);
+    })
+    .catch((err) => console.log(err));
 });
+app.post("/findNotes", (req, res) => {
+  admin
+    .firestore()
+    .collection("notes")
+    .where("subject", "==", req.body.subjet)
+    .limit(5)
+    .get()
+    .then((data) => {
+      let users: any = [];
+      data.forEach((doc) => {
+        users.push({
+          nId: doc.id,
+          ...doc.data(),
+        });
+      });
+      return res.json(users);
+    })
+    .catch((err) => console.log(err));
+});
+app.get("/getAllUsers", (req, res) => {
+  admin
+    .firestore()
+    .collection("users")
+    .get()
+    .then((data) => {
+      let users: any = [];
+      data.forEach((doc) => {
+        users.push({
+          uId: doc.id,
+          ...doc.data(),
+        });
+      });
+
+      return res.json(users);
+    })
+    .catch((err) => console.log(err));
+});
+app.post("/createNotes", (req, res) => {
+  const newNotes = {
+    title: req.body.title,
+    uId: req.body.userId,
+    description: req.body.description,
+    ask: req.body.ask,
+    price: req.body.price,
+    pageNum: req.body.pageNum,
+    created: admin.firestore.Timestamp.fromDate(new Date()),
+  };
+  admin
+    .firestore()
+    .collection("notes")
+    .add(newNotes)
+    .then((doc) =>
+      res.json({ message: `document ${doc.id} created successfully` })
+    )
+    .catch((err) => {
+      res.status(500).json({ error: "could not create notes" });
+      console.error(err);
+    });
+});
+
+// ap
+exports.api = functions.https.onRequest(app);
